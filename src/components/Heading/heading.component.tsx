96de134bcaf51dd8cc3070892ca858068a96de45
NatanTavares <nattantavares.s15@gmail.com>
2022-10-14 22:39:35 -0300

    refactor: updates the types

--- src/components/Heading/heading.component.tsx
+++ src/components/Heading/heading.component.tsx
@@ -3,17 +3,21 @@ import type { HeadingProps } from './heading.types';
 import { clsx } from 'clsx';
 
 export const Heading: React.FC<HeadingProps> = (props) => {
-  const { children, size = 'md', asChild = false } = props;
+  const { children, size = 'md', asChild = false, className } = props;
 
   const Comp = asChild ? Slot : 'h2';
 
   return (
     <Comp
-      className={clsx('text-gray-100 font-sans font-bold', {
-        'text-lg': size === 'sm',
-        'text-xl': size === 'md',
-        'text-2xl': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-sans font-bold',
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg',
+        },
+        className,
+      )}
     >
       {children}
     </Comp>
